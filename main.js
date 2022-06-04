1ededd1532b8f6876eae02a4d9e833e2e15aa84d
Santiago Campuzano <82460097+santiagoSc98@users.noreply.github.com>
2022-06-03 22:21:56 -0400

    implementacion de mysql

--- main.js
+++ main.js
@@ -1,34 +1,61 @@
 const express = require("express");
-const res = require("express/lib/response");
+const bodyParser = require("body-parser")
+// const res = require("express/lib/response");
 const app = express()
 let PORT = process.env.PORT || 3000;
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/DBInmobiliaria";
-// const htmlfile = require(__dirname+"/html/index.html")
+const http = require("http").createServer(app)
 
-function insertUser(){
-    MongoClient.connect(url, function(err, db) {
+app.use(bodyParser.urlencoded({extended:true}));
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "santycampu",
+  database: "inmobiliaria"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+function consult(){
+    console.log("hoals ")
+    con.query("SELECT * FROM usuario;", function(err, result) {
         if (err) throw err;
-        var dbo = db.db("DBInmobiliaria");
-        var myobj = { name: "Rimax", address: "Mcal. Lopez 5678" };
-        dbo.collection("usuario").insertOne(myobj, function(err, res) {
-          if (err) throw err;
-          console.log("1 document inserted");
-          db.close();
-        });
+        console.log("Result: " + result);
       });
 }
 
-app.get("/home", (req, res) => {
-    res.sendFile(__dirname+"/html/index.html")
-});
+ app.get('/', function (req, res) {
+    // consult()
+    res.send("consulta realizada /")
+})
 
-app.get("/insert", (req, res) => {
+app.post('/register', function (req, res) {
+    con.query("INSERT INTO ente (id_tipoente,nombre,telefono,descripcion,direccion,email,documentoidentificador,pass) VALUES (1,'"+ req.body.nombre +"','"+ req.body.telefono +"','el bicho','"+ req.body.direccion +"','"+ req.body.email +"','"+ req.body.dni +"','"+ req.body.pass+"') ;", function(err, result) {
+        if (err) throw err;
+        console.log("Result: " , result);
+      });
+    
+    res.send("register correcto")
+})
+
+app.use(express.static(__dirname + '/html'))
+
+app.get('/home', function(req, res){
+    console.log("holsaas home")
+    res.sendFile(__dirname+'/html/index.html')
+})
+
+app.get('/insert', function(req, res)  {
     insertUser()
     res.send("insert")
-});
+})
 
-app.listen(PORT, () => {
+app.listen(PORT, function(){
     console.log(`App is running on port: ${PORT}`);
-});
+})
