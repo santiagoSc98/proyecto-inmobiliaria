50f9c600437296e6eb8fed1901ba44498a3445d9
Santiago Campuzano <82460097+santiagoSc98@users.noreply.github.com>
2022-06-05 22:21:34 -0400

    Implementacion del login

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser")
-// const res = require("express/lib/response");
 const app = express()
 let PORT = process.env.PORT || 3000;
 
-const http = require("http").createServer(app)
+// const http = require("http").createServer(app)
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -36,7 +35,17 @@ function consult(){
 })
 
 app.post('/register', function (req, res) {
-    con.query("INSERT INTO ente (id_tipoente,nombre,telefono,descripcion,direccion,email,documentoidentificador,pass) VALUES (1,'"+ req.body.nombre +"','"+ req.body.telefono +"','el bicho','"+ req.body.direccion +"','"+ req.body.email +"','"+ req.body.dni +"','"+ req.body.pass+"') ;", function(err, result) {
+
+    let tipo =  req.body.tipo;
+    let nombre =  req.body.nombre;
+    let telefono =  req.body.telefono;
+    let descripcion =  req.body.descripcion;
+    let direccion = req.body.direccion;
+    let email = req.body.email;
+    let dni = req.body.dni;
+    let pass = req.body.pass;
+
+    con.query("INSERT INTO ente (id_tipoente,nombre,telefono,descripcion,direccion,email,documentoidentificador,pass) VALUES ('"+ tipo +"','"+ nombre +"','"+ telefono +"','"+ descripcion +"','"+ direccion +"','"+ email +"','"+ dni +"','"+ pass +"') ;", function(err, result) {
         if (err) throw err;
         console.log("Result: " , result);
       });
@@ -44,10 +53,42 @@ app.post('/register', function (req, res) {
     res.send("register correcto")
 })
 
+var sessionLog = []
+ 
+app.post('/login', function(req, res) {
+
+	let email = req.body.email;
+	let pass = req.body.pass;
+
+	if (email && pass) {
+	
+		con.query('SELECT * FROM ente WHERE email = ? AND pass = ?', [email, pass], function(err, result) {
+			
+			if (err) throw err;
+
+			if (result.length > 0) {
+        sessionLog["id"+result[0].id_ente] = {}
+        sessionLog["id"+result[0].id_ente].logged = true
+        console.log ("sesion", sessionLog);
+        res.send("login correcto");        
+			} else {
+				res.send('Email o contraseña incorrecta!');
+			}			
+			res.end();
+		});
+
+	} else {
+		res.send('Rellene los campoos!');
+		res.end();
+	}
+});
+
+
+
 app.use(express.static(__dirname + '/html'))
 
 app.get('/home', function(req, res){
-    console.log("holsaas home")
+    // console.log("holsaas home")
     res.sendFile(__dirname+'/html/index.html')
 })
 
