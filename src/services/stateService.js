969e2eae4eaa321b406f8b45b09aae7fc5ee7349
MiltonGarcia <milgarsan@gmail.com>
2022-07-06 22:18:14 -0400

    Se configuraron los servicios de propiedad para que devuelvan una promesa, y asi trabajar de forma asincrona

--- src/services/stateService.js
+++ src/services/stateService.js
@@ -1,53 +1,66 @@
 const dbConnection = require("../db/databaseConnection")
 const service = {};
 
-service.list = function () {
-    return dbConnection.query("Select * from propiedad", (error, result, fields) => {
-        try {
-            return result
-        } catch (error) {
-            throw error
-        }
-    });
+service.list = async function () {
+    return new Promise(resolve => {
+        dbConnection.query("SELECT * FROM propiedad", (error, result) => {
+            if (error) {
+                resolve(0)
+                console.log(error)
+            } else {
+                resolve(result)
+            }
+        });
+    })
 }
 
 service.save = function () {
-    return dbConnection.query("INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...)", (error, result, fields) => {
-        try {
-            return result
-        } catch (error) {
-            throw error
-        }
-    });
+    return new Promise(resolve => {
+        dbConnection.query("INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...)", (error, result, fields) => {
+            if (error) {
+                resolve(0)
+                console.log(error)
+            } else {
+                resolve(result)
+            }
+        });
+    })
 }
 
 service.select = function () {
-    return dbConnection.query("SELECT column1, column2, ... FROM table_name WHERE condition", (error, result, fields) => {
-        try {
-            return result
-        } catch (error) {
-            throw error
-        }
-    });
+    return new Promise(resolve => {
+        dbConnection.query("SELECT column1, column2, ... FROM table_name WHERE condition", (error, result, fields) => {
+            if (error) {
+                resolve(0)
+                console.log(error)
+            } else {
+                resolve(result)
+            }
+        });
+    })
 }
 
 service.update = function () {
-    return dbConnection.query("UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition", (error, result, fields) => {
-        try {
-            return result
-        } catch (error) {
-            throw error
-        }
-    });
+    return new Promise(resolve => {
+        dbConnection.query("UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition", (error, result, fields) => {
+            try {
+                return result
+            } catch (error) {
+                throw error
+            }
+        });
+    })
 }
 service.delete = function () {
-    return dbConnection.query("DELETE FROM table_name WHERE condition", (error, result, fields) => {
-        try {
-            return result
-        } catch (error) {
-            throw error
-        }
-    });
+    return new Promise(resolve => {
+        dbConnection.query("DELETE FROM table_name WHERE condition", (error, result, fields) => {
+            try {
+                return result
+            } catch (error) {
+                throw error
+            }
+        });
+    })
 }
 
 module.exports = service
